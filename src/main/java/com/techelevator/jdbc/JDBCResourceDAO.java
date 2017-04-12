9814d3c0cb68020143d7d20279324d7ba29b8545
Alex Khrizman <akhrizman@Techs-MacBook-Air.local>
2017-04-11 23:23:49 -0400

    added 3 DAOs

---
+++ src/main/java/com/techelevator/jdbc/JDBCResourceDAO.java
@@ -0,0 +1,67 @@
+package com.techelevator.jdbc;
+
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.daos.ResourceDAO;
+import com.techelevator.models.Resource;
+
+public class JDBCResourceDAO extends JDBCDAO implements ResourceDAO {
+
+	public JDBCResourceDAO(DataSource dataSource) {
+		super(dataSource);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Resource createNewResource(Resource resource) {
+		long id = getNextResourceId();
+		String sqlCreateResource = "INSERT INTO resources (resourceId, url, title, description, lessonId) VALUES (?,?,?,?,?)";
+		int rowsAffected = jdbcTemplate.update(sqlCreateResource, id, resource.getUrl().toLowerCase(), resource.getTitle(), resource.getDescription(), resource.getLessonId());
+		if (rowsAffected == 1) {
+			resource.setResourceId(id);
+			return resource;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Resource getResourceByResourceId(long id) {
+		String sqlGetResourceById = "SELECT * FROM resources WHERE resource = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetResourceById, id);
+		if(results.next()) {
+			return mapRowToResource(results);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public ArrayList<Resource> getResourcesByLessonId(long lessonId) {
+		ArrayList<Resource> resources = new ArrayList<Resource>();
+		String sqlGetResourcesByLessonId = "SELECT * FROM resources WHERE lessonId = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetResourcesByLessonId, lessonId);
+		while (results.next()) {
+			Resource nextResource = mapRowToResource(results);
+			resources.add(nextResource);
+		}
+		return resources;	}
+
+	private long getNextResourceId() {
+		return super.getNextId("seq_resourceId");
+	}
+
+	private Resource mapRowToResource(SqlRowSet results) {
+		Resource aResource = new Resource();
+		aResource.setResourceId(results.getLong("resourceId"));
+		aResource.setUrl(results.getString("url"));
+		aResource.setTitle(results.getString("title"));
+		aResource.setDescription(results.getString("description"));
+		aResource.setLessonId(results.getLong("lessonId"));
+		return aResource;
+	}
+}
