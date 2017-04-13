19a4d6eff30d828082fef0b634cf5f99efa900c2
Alex Khrizman <akhrizman@Techs-MacBook-Air.local>
2017-04-13 15:40:15 -0400

    added initial authorization

--- src/main/java/com/techelevator/security/AuthorizationFilter.java
+++ src/main/java/com/techelevator/security/AuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.techelevator.security;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,9 +12,26 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.techelevator.daos.CourseDAO;
+import com.techelevator.daos.UserDAO;
+import com.techelevator.jdbc.JDBCCourseDAO;
+import com.techelevator.jdbc.JDBCUserDAO;
+import com.techelevator.models.Course;
+import com.techelevator.models.User;
 
 public class AuthorizationFilter implements Filter {
 
+	@Autowired
+	CourseDAO courseDAO;
+	
+//	@Autowired
+//	UserDAO userDAO;	
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		
@@ -26,16 +44,18 @@ public class AuthorizationFilter implements Filter {
 		HttpServletRequest httpRequest = (HttpServletRequest)request;
 		HttpServletResponse httpResponse = (HttpServletResponse)response;
 		
-		String sessionUser = getUserFromSession(httpRequest);
-		String requestUser = getUserFromRequest(httpRequest);
-		
-		if(requestUser != null && !requestUser.equals(sessionUser)) {
-			if(sessionUser == null) {
+		long sessionUserId = getUserIdFromSession(httpRequest);
+		String requestCourseId = getCourseIdFromRequest(httpRequest);
+				
+		if(!courseDAO.courseHasGivenTeacher(requestCourseId, sessionUserId) 
+				&& !courseDAO.studentIsEnrolledInCourse(requestCourseId, sessionUserId)) {
+			if(sessionUserId == 0) {
 				redirectToLoginPage(httpRequest, httpResponse);
 			} else {
 				httpResponse.sendError(403);
 			}
 		}
+		
 		chain.doFilter(request, response);
 	}
 
@@ -52,19 +72,20 @@ public class AuthorizationFilter implements Filter {
 		httpResponse.sendRedirect(context+"/login?destination="+URLEncoder.encode(originalRequest, "UTF-8"));
 	}
 
-	private String getUserFromSession(HttpServletRequest httpRequest) {
-		return (String)httpRequest.getSession().getAttribute("currentUser");
+	private long getUserIdFromSession(HttpServletRequest httpRequest) {
+		User user = (User)httpRequest.getSession().getAttribute("currentUser");
+		return user.getUserId();
 	}
 
-	private String getUserFromRequest(HttpServletRequest httpRequest) {
-		String requestUser = null;
+	private String getCourseIdFromRequest(HttpServletRequest httpRequest) {
+		String requestCourseId = null;
 		String[] path = httpRequest.getServletPath().split("/");
 		if(path.length >= 3) {
-			if(path[2].equals("new") == false) {
-				requestUser = path[2];
+			if(path[2] != null) {
+				requestCourseId = path[2];
 			}
 		}
-		return requestUser;
+		return requestCourseId;
 	}
 
 	@Override
