26a16a36bae71e29bc97eef919629b50096a1e5e
Alex Khrizman <akhrizman@Techs-MacBook-Air.local>
2017-04-13 13:37:48 -0400

    webxml

--- src/main/java/com/techelevator/security/AuthorizationFilter.java
+++ src/main/java/com/techelevator/security/AuthorizationFilter.java
@@ -29,7 +29,7 @@ public class AuthorizationFilter implements Filter {
 		String sessionUser = getUserFromSession(httpRequest);
 		String requestUser = getUserFromRequest(httpRequest);
 		
-		if(requestUser != null && requestUser.equals(sessionUser) == false) {
+		if(requestUser != null && !requestUser.equals(sessionUser)) {
 			if(sessionUser == null) {
 				redirectToLoginPage(httpRequest, httpResponse);
 			} else {
